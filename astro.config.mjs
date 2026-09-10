d3ee932944d1ee6d6708908dfdf26acce2f203fa
HUgo4294 <victorhernandez9552@gmail.com>
2026-09-10 12:24:21 -0600

    Actualizacion de la Unidad 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,28 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+
   integrations: [
     starlight({
       title: 'Wiki Sistemas Operativos 1',
+
       customCss: ['./src/assets/styles/custom.css'],
+
       sidebar: [
         {
           label: 'Unidad 1',
-          items: [{ autogenerate: { directory: 'unidad-1' } }],
+          items: [
+            { autogenerate: { directory: 'unidad-1' } }
+          ],
         },
         {
           label: 'Unidad 2',
-          items: [{ autogenerate: { directory: 'unidad-2' } }],
+          items: [
+            { autogenerate: { directory: 'unidad-2' } }
+          ],
         },
       ],
     }),
