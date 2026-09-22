51cd911eeac018df4e1168981e4e36a425de2976
HUgo4294 <victorhernandez9552@gmail.com>
2026-09-22 11:46:25 -0600

    Configurar GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,13 @@
-// @ts-check
-
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://hugo4294.github.io',
+  base: '/wiki-sistemas-operativos-',
 
   integrations: [
     starlight({
-      title: 'Wiki Sistemas Operativos 1',
-
-      customCss: ['./src/assets/styles/custom.css'],
-
-      sidebar: [
-        {
-          label: 'Unidad 1',
-          items: [
-            { autogenerate: { directory: 'unidad-1' } }
-          ],
-        },
-        {
-          label: 'Unidad 2',
-          items: [
-            { autogenerate: { directory: 'unidad-2' } }
-          ],
-        },
-      ],
+      title: 'Wiki Sistemas Operativos',
     }),
   ],
 });
