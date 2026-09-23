ff50053b8028945509ee39133df7663fd03e662c
HUgo4294 <victorhernandez9552@gmail.com>
2026-09-23 12:40:56 -0600

    Publicar wiki corregida

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
@@ -7,7 +9,42 @@ export default defineConfig({
 
   integrations: [
     starlight({
-      title: 'Wiki Sistemas Operativos',
+      title: 'Wiki Sistemas Operativos 1',
+
+      customCss: ['./src/assets/styles/custom.css'],
+
+      sidebar: [
+        {
+          label: 'Unidad 1',
+          items: [
+            {
+              autogenerate: {
+                directory: 'unidad-1',
+              },
+            },
+          ],
+        },
+        {
+          label: 'Unidad 2',
+          items: [
+            {
+              autogenerate: {
+                directory: 'unidad-2',
+              },
+            },
+          ],
+        },
+        {
+          label: 'Unidad 3',
+          items: [
+            {
+              autogenerate: {
+                directory: 'unidad-3',
+              },
+            },
+          ],
+        },
+      ],
     }),
   ],
 });
